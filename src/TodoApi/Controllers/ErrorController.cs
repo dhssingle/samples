2fe18cba22581f766fcea4d530930b9b788a9f62
Ding Haishen <dhssingle@gmail.com>
2020-12-07 10:59:47 +0800

    升级 .Net 5

--- src/TodoApi/Controllers/ErrorController.cs
+++ src/TodoApi/Controllers/ErrorController.cs
@@ -4,10 +4,10 @@ using Microsoft.AspNetCore.Mvc;
 namespace TodoApi.Controllers
 {
     [ApiController]
+    [ApiExplorerSettings(IgnoreApi = true)]
     public class ErrorController : ControllerBase
     {
         [Route("/error")]
-        []
         public IActionResult Error() => Problem();
     }
 }
