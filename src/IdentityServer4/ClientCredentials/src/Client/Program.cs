51e4c260c8332592e68e4b3b3c3ca41c9d41ff28
dhssingle <dhssingle@gmail.com>
2021-08-12 14:17:37 +0800

    add ClientCredentials

---
+++ src/IdentityServer4/ClientCredentials/src/Client/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Net.Http;
+using System;
+using IdentityModel.Client;
+
+var client = new HttpClient();
+var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+if (disco.IsError)
+{
+    Console.WriteLine(disco.Error);
+    return;
+}
+//Console.WriteLine(disco.Json);
+
+var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+{
+    Address = disco.TokenEndpoint,
+    ClientId = "client",
+    ClientSecret = "secret",
+    Scope = "api1"
+});
+
+if (tokenResponse.IsError)
+{
+    Console.WriteLine(tokenResponse.Error);
+    return;
+}
+
+Console.WriteLine(tokenResponse.Json);
+
+var apiClient = new HttpClient();
+apiClient.BaseAddress = new Uri("https://localhost:6001");
+apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+var response = await apiClient.GetAsync("identity");
+if (!response.IsSuccessStatusCode)
+{
+    Console.WriteLine(response.StatusCode);
+}
+else
+{
+    var content = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(content);
+}
+
---
+++ src/IdentityServer4/ClientCredentials/src/IdentityServer/Config.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "My API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" }
+                }
+            };
+        }
+        public static IEnumerable<ApiScope> GetScopes()
+        {
+            return new List<ApiScope>
+            {
+                new ApiScope("api1", "api1")
+            };
+        }
+    }
+}
